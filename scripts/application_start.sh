30c9727bd0fd36cf89ef00724f28520a352d8631
chavdaAbhishek <chavda.abhishek55@gmail.com>
2021-12-27 17:19:08 +0530

    final release

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #give permission for everything in the express-app directory
-sudo chmod -R 777 /home/ec2-user/express-app
+sudo chmod -R 777 /home/ec2-user/express-demo-app
 
 #navigate into our working directory where we have all our github files
 cd /home/ec2-user/express-app
@@ -15,4 +15,4 @@ export NVM_DIR="$HOME/.nvm"
 npm install
 
 #start our node app in the background
-node app.js > app.out.log 2> app.err.log < /dev/null & 
+npm start > app.out.log 2> app.err.log < /dev/null & 
